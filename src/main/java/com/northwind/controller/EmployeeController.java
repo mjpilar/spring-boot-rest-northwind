08dd7474d46c0a7ea63aa381ac8c54920507cc4c
Asus <Asus@DESKTOP-N2D7RE3>
2019-08-11 21:27:58 +0800

    Added Get Employee by Id Resource

--- src/main/java/com/northwind/controller/EmployeeController.java
+++ src/main/java/com/northwind/controller/EmployeeController.java
@@ -1,8 +1,11 @@
 package com.northwind.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,13 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	@RequestMapping("/employees")
+	@GetMapping("/employees")
 	public List<Employee> getEmployees() {
 		return employeeRepository.findAll();
+	}
+	
+	@GetMapping("/employees/{id}")
+	public Optional<Employee> getEmployee(@PathVariable Long id) {
+		return employeeRepository.findById(id);
 	}
 }
