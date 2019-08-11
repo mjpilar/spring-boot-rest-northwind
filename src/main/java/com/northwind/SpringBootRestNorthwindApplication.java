1fb07b96add33dbb7c9b2972e8f8e6f28e82d019
Asus <Asus@DESKTOP-N2D7RE3>
2019-08-11 16:37:20 +0800

    Initial Commit

---
+++ src/main/java/com/northwind/SpringBootRestNorthwindApplication.java
@@ -0,0 +1,13 @@
+package com.northwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestNorthwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestNorthwindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northwind/SpringBootRestNorthwindApplicationTests.java
@@ -0,0 +1,16 @@
+package com.northwind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestNorthwindApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
